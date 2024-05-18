9fe71ebb23ed8fc2f7f2e40bfe27106609b987bb
SunYerim <yaelim6@pukyong.ac.kr>
2024-05-19 00:41:22 +0900

    [FEATURE] 댓글 등록 성공
    댓글 등록 및 폼 생성

--- application/src/components/qna/QnaDetail.vue
+++ application/src/components/qna/QnaDetail.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { ref, onBeforeMount, onMounted, computed } from "vue";
+import { ref, onBeforeMount, reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { getArticle, deleteArticle } from "@/api/board";
 import noAuthClient from "@/api/noAuthClient";
 import Cookies from "vue-cookies";
 import axios from "axios";
@@ -11,6 +10,11 @@ const router = useRouter();
 
 const qna = ref({});
 const comments = ref([]);
+
+// data
+const newComment = reactive({
+  commentContent: "",
+});
 
 const qnaId = route.params.qnaId;
 const goModify = () => {
@@ -23,18 +27,18 @@ const goModify = () => {
 const deleteQna = async () => {
   try {
     const token = Cookies.get("accessToken");
-    const res = await axios({
+    await axios({
       method: "delete",
       url: `${import.meta.env.VITE_API_BASE_URL}/qna/${qnaId}`,
       headers: {
         Authorization: `Bearer ${token}`,
-      }
-    })
+      },
+    });
     alert("삭제에 성공하였습니다.");
     goList();
   } catch (error) {
     console.log(error);
-    alert("작성자만 글을 삭제할 수 있습니다.")
+    alert("작성자만 글을 삭제할 수 있습니다.");
   }
 };
 
@@ -44,22 +48,46 @@ const goList = () => {
   });
 };
 
+const registerComment = async () => {
+  try {
+    const token = Cookies.get("accessToken");
+    await axios({
+      method: "post",
+      url: `${import.meta.env.VITE_API_BASE_URL}/qna/comment/regist`,
+      data: {
+        articleId: qnaId,
+        content: newComment.commentContent,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    alert("댓글 등록에 성공하였습니다.");
+    newComment.commentContent = "";
+    await fetchComments(); // 댓글 목록 갱신
+  } catch (error) {
+    console.log(error);
+    alert("댓글 등록에 실패하였습니다.");
+  }
+};
 
-
-// 게시글 상세 조회
-onBeforeMount( async () => {
-  try { 
+const fetchComments = async () => {
+  try {
     const res = await noAuthClient({
       method: "get",
       url: `${import.meta.env.VITE_API_BASE_URL}/qna/view/${qnaId}`,
-    })
-    console.log(res.data);
+    });
     qna.value = res.data;
     comments.value = res.data.list;
   } catch (error) {
     console.log(error);
-    alert("문제가 발생했습니다.")
+    alert("문제가 발생했습니다.");
   }
+};
+
+// 게시글 상세 조회 및 댓글 목록 조회
+onBeforeMount(async () => {
+  await fetchComments();
 });
 </script>
 
@@ -72,16 +100,14 @@ onBeforeMount( async () => {
         </h2>
       </div>
       <div class="col-lg-12">
-        <h1 class="">{{ qna.qnaTitle }}</h1>
+        <h1>{{ qna.qnaTitle }}</h1>
         <div class="d-flex justify-content-between">
           <div>
             <label for="writer">작성자 :</label>
             {{ qna.qnaWriter }}
           </div>
-
           <div>
             {{ qna.qnaCreateDate }}
-
             <label class="ms-1" for="view">조회 </label>
             {{ qna.qnaView }}
           </div>
@@ -98,19 +124,36 @@ onBeforeMount( async () => {
           >
           <a
             class="btn btn-outline-danger ms-2 pe-4 ps-4"
-            @click="deleteQna()"
+            @click="deleteQna"
             >삭제</a
           >
           <button class="btn btn-outline-dark ms-2 pe-4 ps-4" @click="goList">
             목록
           </button>
         </div>
-        <hr />
-        <ul>
-            <li v-for="comment in comments" :key="comment.qnaCommentId">
-              <strong>{{ comment.userId }}:</strong> {{ comment.content }}
-            </li>
-          </ul>
+        <h3>댓글</h3>
+        <form @submit.prevent="registerComment">
+          <div class="mb-3">
+            <label for="newComment" class="form-label">댓글 작성</label>
+            <textarea
+              class="form-control"
+              id="newComment"
+              rows="3"
+              v-model="newComment.commentContent"
+            ></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">댓글 등록</button>
+        </form>
+        <hr/>
+        <ul class="list-group mb-3">
+          <li
+            class="list-group-item"
+            v-for="comment in comments"
+            :key="comment.qnaCommentId"
+          >
+            <strong>{{ comment.userId }}:</strong> {{ comment.content }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
