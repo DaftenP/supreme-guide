d89150bfb220b812ce2047a39251f1d677f0ff05
DaftenP <80443975+DaftenP@users.noreply.github.com>
2024-05-21 09:48:43 +0900

    #35 refactor: DTO 변경 및 실행 코드

--- server-api/main.py
+++ server-api/main.py
@@ -1,4 +1,6 @@
 from typing import Optional
+
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from model import NLPModel
@@ -9,20 +11,23 @@ model = NLPModel('model_10.pt')
 
 # Pydantic을 사용하여 요청 데이터의 구조를 정의합니다.
 class Article(BaseModel):
-    board_id: int
-    board_title: Optional[str] = None
-    board_content: Optional[str] = None
-    board_view: int
-    board_writer: Optional[str] = None
-    board_created_time: Optional[str] = None
+    id: Optional[int] = None
+    articleId: int
+    content: str
+    userId: Optional[str] = None
+    userName: Optional[str] = None
+    create_date: Optional[str] = None
+    harmful: Optional[bool] = None
 
 
 @app.post("/check")
 async def check(article: Article):
-    if not model.sentence_predict(article.board_title):
-        return {'msg': f'부적절한 제목 입니다. ({article.board_title})'}
-    content_sentence = list(article.board_content.split("\n"))
+    content_sentence = list(article.content.split("\n"))
     for sentence in content_sentence:
         if not model.sentence_predict(sentence):
             return {'msg': f'부적절한 내용 입니다. ({sentence})'}
     return {"msg": ""}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
