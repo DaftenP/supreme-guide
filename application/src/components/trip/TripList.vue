f30c382341717316d353a34cdd14681daa4fe471
DaftenP <80443975+DaftenP@users.noreply.github.com>
2024-05-23 06:43:52 +0900

    #47 style: 여행 계획 리스트 View 스타일 적용

--- application/src/components/trip/TripList.vue
+++ application/src/components/trip/TripList.vue
@@ -79,76 +79,80 @@ const goDetail = (id) => {
 
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-12">
-        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
-          <mark class="sky">여행</mark>
-        </h2>
+    <div class="row justify-content-center my-4">
+      <div class="col-lg-8">
+        <h2 class="text-center font-style">여행</h2>
       </div>
-      <div class="col-lg-12">
-        <div class="row align-self-center mb-2">
-          <div class="col-md-2 text-start">
-            <button
-              type="button"
-              class="btn btn-outline-primary btn-sm"
-              v-if="userStore.userId != ''"
-              @click="movePage">
-              여행 계획 등록
-            </button>
-          </div>
-          <div class="col-md-5 offset-5">
-            <form class="d-flex">
-              <div class="input-group input-group-sm">
-                <select class="form-select" v-model="key">
-                  <option value="trip_name">제목</option>
-                  <option value="trip_content">내용</option>
-                  <option value="user_name">작성자</option>
-                </select>
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="검색어"
-                  v-model="word" />
-                <button
-                  class="btn btn-dark"
-                  type="button"
-                  @click="searchArticle">
-                  검색
-                </button>
+      <div class="d-flex column p-2">
+        <div class="col-6"></div>
+        <div class="col-6 d-flex">
+          <select class="form-select" v-model="key">
+            <option value="trip_title">제목</option>
+            <option value="trip_content">내용</option>
+            <option value="trip_writer">작성자</option>
+          </select>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="검색어를 입력해 주세요"
+            v-model="word" />
+          <button class="btn btn-dark" type="button" @click="searchArticle">
+            <font-awesome-icon
+              :icon="['fas', 'magnifying-glass']"
+              style="color: #fcfcfc" />
+          </button>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-md-4 mb-4"
+          v-for="(trip, index) in trips"
+          :key="trip.tripId"
+          :index="index">
+          <div
+            class="card h-100"
+            @click="goDetail(trip.tripId)"
+            :data-aos="'fade-up'"
+            :data-aos-delay="index * 100">
+            <div class="card-body">
+              <h5 class="card-title">
+                {{ trip.tripName }}
+              </h5>
+
+              <!-- {{ trip.tripContent }} -->
+              <br />
+              <br />
+              <div class="card-text d-flex">
+                <p>
+                  {{
+                    new Date(trip.tripCreateDate).toLocaleDateString("ko-KR")
+                  }}
+                </p>
               </div>
-            </form>
+              <p>
+                {{ trip.userId }}
+              </p>
+            </div>
           </div>
         </div>
-        <table class="table table-hover">
-          <thead>
-            <tr class="text-center">
-              <th scope="col">제목</th>
-              <th scope="col">조회수</th>
-              <th scope="col">작성자</th>
-              <th scope="col">작성일</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              class="text-center"
-              v-for="(trip, index) in trips"
-              :index="index"
-              :trip="trip"
-              :key="trip.tripId">
-              <td>
-                <a @click="goDetail(trip.tripId)">{{ trip.tripName }}</a>
-              </td>
-              <td>{{ trip.tripView }}</td>
-              <td>{{ trip.userName }}</td>
-              <td>{{ trip.tripCreateDate }}</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
-    <nav aria-label="Page navigation example">
+    <div class="col-md-2 text-start">
+      <button
+        type="button"
+        class="btn btn-sm"
+        id="font-small"
+        v-if="userStore.userId != ''"
+        @click="movePage">
+        질문 등록
+        <font-awesome-icon
+          :icon="['fas', 'arrow-right']"
+          style="color: black" />
+      </button>
+    </div>
+
+    <nav aria-label="Page navigation example" class="mt-4">
       <ul class="pagination justify-content-center">
-        <!-- 이전 버튼 -->
         <li class="page-item" :class="{ disabled: currentPage === 1 }">
           <button
             class="page-link"
@@ -157,7 +161,6 @@ const goDetail = (id) => {
             <span aria-hidden="true">&laquo;</span>
           </button>
         </li>
-        <!-- 페이지 번호 -->
         <li
           class="page-item"
           v-for="pageNumber in totalPages"
@@ -167,7 +170,6 @@ const goDetail = (id) => {
             {{ pageNumber }}
           </button>
         </li>
-        <!-- 다음 버튼 -->
         <li class="page-item" :class="{ disabled: currentPage === totalPages }">
           <button
             class="page-link"
@@ -185,5 +187,78 @@ const goDetail = (id) => {
 /* 페이지 처리 스타일 */
 .pagination {
   margin-top: 20px;
+}
+
+.card {
+  position: relative; /* 아이콘을 상대적으로 위치시키기 위해 설정 */
+  transition: all 0.3s ease;
+  cursor: pointer;
+  max-width: 300px;
+  border: none;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: white;
+  border: 1px solid #ddd;
+}
+
+.card-container {
+  /* max-width: 800px; 원하는 최대 너비 설정 */
+  /* margin: 0 auto; 가운데 정렬 */
+  max-width: 1200px;
+  flex-wrap: wrap;
+  display: flex;
+  gap: 1rem;
+}
+
+.font-style {
+  font-family: "CustomFont";
+  font-size: 50px;
+}
+
+#font-small {
+  font-family: "CustomFont3";
+  font-size: 20px;
+}
+
+.container {
+  max-width: 1000px;
+}
+
+.card:hover {
+  transform: translateY(-10px);
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.card-icon {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 2.5rem; /* 아이콘 크기 조정 */
+  color: #63e6be;
+}
+
+.card-title {
+  color: #333;
+  font-weight: bold;
+  font-family: "CustomFont";
+  font-size: 25px;
+}
+
+.card-title:hover {
+  text-decoration: underline;
+}
+
+.card-body {
+  padding: 20px;
+}
+
+.card-text p {
+  margin-bottom: 0;
+  color: #666;
+}
+
+.card-text p:last-child {
+  margin-top: 10px;
+  color: #999;
 }
 </style>
